d8b9d480cff65ed907df4311010cdbb8e810de77
SangHoon <tkdgns7070@gmail.com>
2024-08-06 14:57:50 +0900

    docs: Swagger 작성

--- src/main/java/layeredarchitecture/architecture/presentation/AuthenticationController.java
+++ src/main/java/layeredarchitecture/architecture/presentation/AuthenticationController.java
@@ -1,15 +1,29 @@
 package layeredarchitecture.architecture.presentation;
 
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import layeredarchitecture.architecture.application.AuthenticationService;
 import layeredarchitecture.architecture.domain.JsonWebToken;
 import layeredarchitecture.architecture.presentation.response.AuthResponse;
+import layeredarchitecture.architecture.presentation.response.ErrorResponse;
 import layeredarchitecture.common.dto.AuthDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
+@Tag(
+        name = "JWT API",
+        description = "JWT 관련 API"
+)
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -25,7 +39,35 @@ public class AuthenticationController {
      * @param authDto 인증 정보
      * @return ResponseEntity<AuthResponse>
      */
-    @PostMapping
+    @Operation(
+            summary = "JWT 발급",
+            description = "JWT 를 발급 받는다."
+    )
+    @ApiResponses(
+            {@ApiResponse(
+                    responseCode = "200",
+                    description = "JWT 발급 성공",
+                    content = @Content(
+                            examples = @ExampleObject(value = "{\"jwt\":\"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJHUkVFTk5FVCIsImlhdCI6MTcyMjkyMzI1MSwiZXhwIjoxNzIyOTI2ODUxfQ.vl_MWoO5afqupgp-WcLioglI1DIKc-Dm7tuIDHbIU6n3MfUQibSmC_4GMS011ik8LAOaPVDwEnGksrdO7lxbPg\"}"),
+                            schema = @Schema(implementation = Json.class)
+                    )
+            ), @ApiResponse(
+                    responseCode = "404",
+                    description = "클라이언트 시스템 없음",
+                    content = @Content(
+                            examples = @ExampleObject(value = "{\"type\":\"/errors/custom\",\"title\":\"Custom Error\",\"status\":404,\"detail\":\"존재하지 않는 클라이언트 시스템 입니다.\",\"instance\":\"/auth\"}"),
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            ), @ApiResponse(
+                    responseCode = "401",
+                    description = "아이디 또는 비밀번호 불일치",
+                    content = @Content(
+                            examples = @ExampleObject(value = "{\"type\":\"/errors/custom\",\"title\":\"Custom Error\",\"status\":401,\"detail\":\"아이디 또는 비밀번호가 일치하지 않습니다.\",\"instance\":\"/auth\"}"),
+                            schema = @Schema(implementation = ErrorResponse.class)
+                    )
+            )}
+    )
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthResponse> generatedJwt(@RequestBody AuthDto authDto) {
 
         String jwt = authenticationService.generatedJwt(authDto);
@@ -41,7 +83,24 @@ public class AuthenticationController {
      * @param authHeader 인증 정보
      * @return ResponseEntity<AuthResponse>
      */
-    @GetMapping("/check")
+    @Operation(
+            summary = "JWT 유효성 확인",
+            description = "JWT 유효성을 확인한다."
+    )
+    @ApiResponses(
+            {@ApiResponse(
+                    responseCode = "200",
+                    description = "JWT 유효",
+                    content = @Content(
+                            examples = @ExampleObject(value = "{\"isValid\":true}"),
+                            schema = @Schema(implementation = Json.class)
+                    )
+            )}
+    )
+    @GetMapping(
+            value = "/check",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
     public ResponseEntity<AuthResponse> getId(@RequestHeader("Authorization") String authHeader) {
         String token = authHeader.replace("Bearer ", "");
 
--- src/main/java/layeredarchitecture/architecture/presentation/ConsumerController.java
+++ src/main/java/layeredarchitecture/architecture/presentation/ConsumerController.java
@@ -14,10 +14,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,14 +40,14 @@ public class ConsumerController {
                     responseCode = "200",
                     description = "고객 정보 조회 성공",
                     content = @Content(
-                            examples = @ExampleObject(value = "{\"id\": \"1\", \"name\": \"상훈\"}"),
+                            examples = @ExampleObject(value = "{\"id\":\"psh\",\"name\":\"상훈\",\"age\":33,\"money\":10000000}"),
                             schema = @Schema(implementation = ConsumerDto.class)
                     )
             ), @ApiResponse(
                     responseCode = "404",
-                    description = "데이터 없음",
+                    description = "고객 정보 없음",
                     content = @Content(
-                            examples = @ExampleObject(value = "{\"status\":\"NOT_FOUND\", \"message\": \"존재하지 않는 고객 ID 입니다.\"}"),
+                            examples = @ExampleObject(value = "{\"type\": \"/errors/custom\",\"title\": \"Custom Error\",\"status\": 404,\"detail\": \"존재하지 않는 고객 ID 입니다.\",\"instance\": \"/consumers/psh\"}"),
                             schema = @Schema(implementation = ErrorResponse.class)
                     )
             )}
@@ -63,10 +61,23 @@ public class ConsumerController {
         return ResponseEntity.ok(consumerDto);
     }
 
+    @Operation(
+            summary = "전체 고객 정보 조회",
+            description = "전체 고객 정보를 조회하여 반환한다. (by myBatis)"
+    )
     @GetMapping
     public ResponseEntity<List<ConsumerDto>> getConsumers() {
         List<ConsumerDto> consumerDto = consumerService.getConsumers();
 
+        return ResponseEntity.ok(consumerDto);
+    }
+
+    @Operation(
+            summary = "고객 회원 가입 조회",
+            description = "회원 가입을 한다. (파라미터 유효성 검사용)"
+    )
+    @PostMapping
+    public ResponseEntity<ConsumerDto> registerConsumer(@RequestBody @Validated ConsumerDto consumerDto) {
         return ResponseEntity.ok(consumerDto);
     }
 
