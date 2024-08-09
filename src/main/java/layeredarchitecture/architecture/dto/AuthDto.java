b6963847a051c55a6bcb1688fb36ef51ec635798
SangHoon <tkdgns7070@gmail.com>
2024-08-09 09:35:06 +0900

    refactor: dto 위치 변경

--- src/main/java/layeredarchitecture/architecture/dto/AuthDto.java
+++ src/main/java/layeredarchitecture/architecture/dto/AuthDto.java
@@ -1,4 +1,4 @@
-package layeredarchitecture.common.dto;
+package layeredarchitecture.architecture.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
